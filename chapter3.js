78f9d55f94e9d8483f234af444bc65e85a5917e0
Ryan Linton <ryanlntn@gmail.com>
2013-04-17 21:51:04 -0700

    add to chapter 3

--- chapter3.js
+++ chapter3.js
@@ -1,2 +1,17 @@
 // Chapter 3
 // Types, Values, and Variables
+
+// Computer programs work by manipulating values (e.g. 3.14, "Hello World")
+
+// Those values can be stored in variables (e.g. var x = 5)
+
+// Those values which can be represented and manipulated in programming
+// languages are known as types.
+// JavaScript types can be divided into primitives and objects.
+// Primitives include numbers, strings, and booleans as well as null and undefined.
+// Objects include everything else including arrays and functions.
+// JavaScript also includes the Date, Regex, and Error classes.
+
+// JavaScript is an Object-Oriented language
+  // a.sort(); // The object-oriented version of sort(a).
+// In JavaScript, null and undefined are the only values that methods cannot be invoked on.
