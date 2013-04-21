344b0dbf14e8e8a472467c84f308bdf75a2883d4
Ryan Linton <ryanlntn@gmail.com>
2013-04-20 19:37:33 -0700

    add to chapter 4

--- chapter4.js
+++ chapter4.js
@@ -3,4 +3,26 @@
 
 // An expression is a phrase of JavaScript that a JavaScript interpreter can evaluate to produce a value.
 
+// The most common way to build a complex expression out of simpler expressions is with an operator.
+// An operator combines the values of its operands (usually two of them) in some way and evaluates to a new value.
+
 // Primary Expressions
+  // The simplest expressions, known as primary expressions, are those that stand alone
+  // they do not include any simpler expressions. Primary expressions in JavaScript are
+  // constant or literal values, certain language keywords, and variable references.
+
+  // Literals
+    1.23 // A number literal
+    "hello" // A string literal
+    /pattern/ // A regular expression literal
+
+  // (some) Reserved Words
+    true // Evalutes to the boolean true value
+    false // Evaluates to the boolean false value
+    null // Evaluates to the null value
+    this // Evaluates to the "current" object
+
+  // Bare Variable References
+    i // Evaluates to the value of the variable i.
+    sum // Evaluates to the value of the variable sum.
+    undefined // undefined is a global variable, not a keyword like null.
