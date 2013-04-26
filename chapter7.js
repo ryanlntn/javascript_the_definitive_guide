b60a92a068c6b5d5e4a95591bdfe856a34978989
Ryan Linton <ryanlntn@gmail.com>
2013-04-26 16:50:05 -0700

    add to chapter 7

--- chapter7.js
+++ chapter7.js
@@ -42,3 +42,14 @@
     0 in a2           // => false: a2 has no element with index 0
 
 // Array Length
+  // Every array has a length property, and it is this property that makes arrays different from regular JavaScript objects.
+    [].length             // => 0: the array has no elements
+    ['a','b','c'].length // => 3: highest index is 2, length is 3
+
+  // You can remove elements from the end of an array by setting the length.
+    a = [1,2,3,4,5]; // Start with a 5-element array.
+    a.length = 3;    // a is now [1,2,3].
+    a.length = 0;    // Delete all elements. a is [].
+    a.length = 5;    // Length is 5, but no elements, like new Array(5)
+
+// Adding and Deleting Array Elements
