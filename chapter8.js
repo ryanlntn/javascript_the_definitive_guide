e702f7da988dee55a8a81ad9fe938779d2ac43f7
Ryan Linton <ryanlntn@gmail.com>
2013-04-28 17:43:03 -0700

    add to chapter 8

--- chapter8.js
+++ chapter8.js
@@ -1,2 +1,8 @@
 // Chapter 8
 // Functions
+
+// A function is a block of JavaScript code that is defined once but may be executed, or invoked, any number of times.
+
+// Defining Functions
+  // Functions are defined with the function keyword
+  // Functions with no return value are sometimes called procedures.
