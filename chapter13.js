62566a43758306016df11ff6340c93c35542ec5c
Ryan Linton <ryanlntn@gmail.com>
2013-05-04 19:30:59 -0700

    add to chapter 13

--- chapter13.js
+++ chapter13.js
@@ -1,2 +1,13 @@
 // Chapter 13
-//
+// JavaScript in Web Browsers
+
+// One of the most important properties of the Window object is document:
+// it refers to a Document object that represents the content displayed in the window.
+
+// One of the most important event handlers is the onload handler of the Window object.
+// It is triggered when the content of the document displayed in the window is stable and ready to be manipulated.
+
+// The use of HTML event handler attributes is considered poor style by many web
+// developers who prefer to keep content and behavior separate.
+
+// There is no formal definition of a program in client-side JavaScript.
