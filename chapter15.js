7e57288298c111c829ea1265a3cf02e397f3638c
Ryan Linton <ryanlntn@gmail.com>
2013-05-06 13:35:19 -0700

    add to chapter 15

--- chapter15.js
+++ chapter15.js
@@ -1,1 +1,5 @@
 // Chapter 15
+// Scripting Documents
+
+// Client-side JavaScript exists to turn static HTML documents into interactive web applications.
+// Scripting the content of web pages is the central purpose of JavaScript.
