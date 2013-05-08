e069f996c9a121c7a1198abc6e43202ae3be6872
Ryan Linton <ryanlntn@gmail.com>
2013-05-07 17:28:12 -0700

    add to chapter 15

--- chapter15.js
+++ chapter15.js
@@ -3,3 +3,14 @@
 
 // Client-side JavaScript exists to turn static HTML documents into interactive web applications.
 // Scripting the content of web pages is the central purpose of JavaScript.
+
+// The DOM defines a number of ways to select elements; you can query a document for an element or elements:
+// • with a specified id attribute;
+// • with a specified name attribute;
+// • with the specified tag name;
+// • with the specified CSS class or classes; or
+// • matching the specified CSS selector
+
+// Nodes can be traversed like a family tree (i.e. parent, child, sibling, etc.)
+
+// HTML elements consist of a tag name and a set of name/value pairs known as attributes.
